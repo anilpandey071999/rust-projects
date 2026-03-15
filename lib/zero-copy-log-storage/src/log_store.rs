a6eb9ff32f941ba842890fe628e6fe35804a5793
anilpandey071999 <anilpandey071999@gmail.com>
2026-03-15 17:22:57 +0530

    feat: add unsafe for multi threaded environment

--- lib/zero-copy-log-storage/src/log_store.rs
+++ lib/zero-copy-log-storage/src/log_store.rs
@@ -1,30 +1,47 @@
 use crate::log_snapshot::LogSnapShot;
+use std::{cell::UnsafeCell, mem::MaybeUninit, sync::atomic::AtomicUsize};
 
+const MAX_ENTRIES: usize = 1000000;
 /// `LogStore` stores
 /// data inside the vec of u8
 /// metadata is help for calculation the offset and len for retivel
 #[derive(Debug, Default)]
 pub struct LogStore {
-    data: Vec<u8>,            // data
-    metadata: Vec<EntryMeta>, // location
+    data: UnsafeCell<Vec<u8>>,            // data
+    metadata: Vec<UnsafeCell<EntryMeta>>, // location
+    entry_count: AtomicUsize,
+    data_pos: AtomicUsize,
 }
 
 impl LogStore {
     pub fn new() -> Self {
         Self {
-            data: Vec::with_capacity(1000000),
-            metadata: Vec::with_capacity(1000000),
+            data: Vec::with_capacity(MAX_ENTRIES),
+            metadata: Vec::with_capacity(MAX_ENTRIES),
+            entry_count: AtomicUsize::new(0),
+            data_pos: AtomicUsize::new(0),
         }
     }
 
-    pub fn append(&mut self, data: &str) -> usize {
-        let new_offset = self.data.len();
-        self.data.extend_from_slice(data.as_bytes());
+    pub fn append(&self, data: &str) -> usize {
+        let index = self.entry_count.load(std::sync::atomic::Ordering::Relaxed);
+        let offset = self
+            .data_pos
+            .fetch_add(data.len(), std::sync::atomic::Ordering::Relaxed);
+        let bytes = data.as_bytes();
 
-        let new_len = data.len();
-        let meta_data = EntryMeta::new(new_offset, new_len);
-        self.metadata.push(meta_data);
-        self.metadata.len() - 1
+        unsafe {
+            let data_buf = &mut *self.data.get();
+            data_buf[offset..offset + bytes.len()].copy_from_slice(bytes);
+        }
+        unsafe {
+            *self.metadata[index].get() = EntryMeta::new(offset, bytes.len());
+        }
+
+        self.entry_count
+            .store(index + 1, std::sync::atomic::Ordering::Release);
+
+        index
     }
 
     pub fn get(&self, log_id: usize) -> Option<&str> {
@@ -38,9 +55,9 @@ impl LogStore {
         None
     }
 
-    pub fn snapshot(&self) -> LogSnapShot<'_> {
-        LogSnapShot::new(&self.data, &self.metadata)
-    }
+    // pub fn snapshot(&self) -> LogSnapShot<'_> {
+    //     LogSnapShot::new(&self.data, &self.metadata)
+    // }
 }
 
 #[derive(Debug, Default, Clone, Copy)]
